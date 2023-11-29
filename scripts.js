5e5531b3da27f81a6742e2f344d48c21d3f70dbd
Caua1dev <cauagooficial@gmail.com>
2023-11-29 14:02:09 -0400

    alteraçao 2

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
 const convertButton = document.querySelector(".convert-button")
 
-function convertValues(){
-    const inputCurrencyValue = document.querySelector("input-currency").value
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
 
-    const dolarToday = 5.2
+    const currencyvaluetoconvert = Document.querySelector(".currency-value-to-conver")//valor em real
 
-    const convertValues = inputCurrencyValue / dolarToday
+    const currencyvalueConverted = Document.querySelector(".currency-value")//outras moedas
 
-    console.log(inputCurrencyValue)
+    const dolarToday = 4.9
+
+
+    const convertValue = inputCurrencyValue / dolarToday
     
+    currencyvaluetoconvert.innerHTML =  new Intl.NumberFormat("pt-BR",{
+        style:"currency",
+        currency:"BRL"
+      }) .format(inputCurrencyValue)
+
+      currencyvalueConverted.innerHTML = new Intl.NumberFormat("en-US",{
+        style:"currency",
+        currency:"USD"
+      }).format(convertValue)
 }
-convertButton.addEventListener("click",convertValues)
+convertButton.addEventListener("click", convertValues)
